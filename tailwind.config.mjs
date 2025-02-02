77539fbe44bf9cb5f3b3e308a711d018cb14e375
Faprin <jmdominguezsegura@gmail.com>
2025-02-02 21:37:29 +0100

    home y about responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,9 @@ const { addDynamicIconSelectors } = require('@iconify/tailwind')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			scrollBehavior: ['smooth'],
+		},
 	},
 	plugins: [
 		addDynamicIconSelectors(),
